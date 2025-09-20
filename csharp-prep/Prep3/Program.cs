78cc409963e954a113564e49e4fb0ef15d6421e5
myciah <rus21019@byui.edu>
2025-09-19 21:33:52 -0600

    Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomNumber = new Random();
+        int magic = randomNumber.Next(1, 101);
+        int number = -1;
+
+        while (magic != number)
+        {
+            Console.Write("What is your guess? ");
+            number = int.Parse(Console.ReadLine());
+            if (magic == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (magic > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+        }
+
     }
 }
