cc8f8a52da40aded7a73a1605ba8657abf335bd9
myciah <rus21019@byui.edu>
2025-09-19 22:11:53 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        }
+        while (input != 0);
+
+        int totalSum = 0;
+        foreach (int number in numbers)
+        {
+            totalSum += number;
+        }
+
+        double average = (numbers.Count > 0) ? (double)totalSum / numbers.Count : 0;
+
+        int max = (numbers.Count > 0) ? numbers[0] : 0;
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {totalSum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
