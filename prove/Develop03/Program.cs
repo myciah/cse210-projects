48d92c0b4bb63df8c1e1fb47911c55c684cf1b50
myciah <rus21019@byui.edu>
2025-10-07 23:45:03 -0600

    prove03 commit3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference ref1 = new Reference("John", 3, 16);
+        Console.WriteLine(ref1.GetDisplayText());
+
+        Reference ref2 = new Reference("Proverbs", 3, 5, 6);
+        Console.WriteLine(ref2.GetDisplayText());
     }
 }
