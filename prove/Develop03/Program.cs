1b8197842e1febe2964d58e6ccafdbee820bf7b7
myciah <rus21019@byui.edu>
2025-10-08 18:25:45 -0600

    prove03 commit10

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,19 +1,37 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Reference ref1 = new Reference("John", 3, 16);
-        Console.WriteLine(ref1.GetDisplayText());
+        // Reference ref1 = new Reference("John", 3, 16);
+        // Console.WriteLine(ref1.GetDisplayText());
 
         Reference ref2 = new Reference("Proverbs", 3, 5, 6);
-        Console.WriteLine(ref2.GetDisplayText());
+        //Console.WriteLine(ref2.GetDisplayText()); (was part of testing at first)
+        string verseT = "Trust in the Lord with all thine heart and lean not unto thine own understanding."
 
-        //testint word class
-        Word word = new Word("Hope");
-        Console.WriteLine(word.GetDisplayText());
-        word.Hide();
-        Console.WriteLine(word.GetDisplayText());
+        Scripture scripture = new Scripture(Reference, verseT);
+        while (!scripture.AllWordHid())
+        {
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("Press Enter to hide more words or type "quit" to exit: ");
+            string inp = Console.ReadLine();
+            if (inp() == "quit")
+            {
+                break;
+            }
+            scripture.HideRanWord(3);
+        }
+        Console.Clear();
+        Console.WriteLine("All the words have been hidden, hope you were able to leanr the scripture!");
+
+        //testing word class
+        //Word word = new Word("Hope");
+        //Console.WriteLine(word.GetDisplayText());
+        //word.Hide();
+        //Console.WriteLine(word.GetDisplayText());
     }
 }
