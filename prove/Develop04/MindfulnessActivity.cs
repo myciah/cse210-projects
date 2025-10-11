b7fb3e05e2070d24deaebe493ade0ffed20f0742
myciah <rus21019@byui.edu>
2025-10-10 21:32:54 -0600

    prove04 commit4

--- prove/Develop04/MindfulnessActivity.cs
+++ prove/Develop04/MindfulnessActivity.cs
@@ -4,17 +4,24 @@ public class MindfulnessActivity
     public string name;
     public string description;
     public int duration;
+    public MindfulnessActivity(string activityName)
+    {
+        name = activityName;
+        description = "Testing";
+    }
     public void Start()
     {
-        Console.WriteLine($"Welcome to {name} activity.")
+        Console.WriteLine($"Welcome to {name} activity.");
         Console.WriteLine(description);
         Console.WriteLine($"In seconds, how long would you like to do this activity for: ");
-        duration = int Console.ReadLine();
-    Console.WriteLine("The activity will start soon. Get Ready!!!");
+        duration = Convert.ToInt32(Console.ReadLine());
+        //duration = int Console.ReadLine();
+        Console.WriteLine("The activity will start soon. Get Ready!!!");
+        Thread.Sleep(2000);
     }
     public void End()
     {
         Console.WriteLine("Great Job!");
-        Console.WriteLine($"You did {name} activity for {duration} seconds. Well done!")
+        Console.WriteLine($"You did {name} activity for {duration} seconds. Well done!");
     }
 }
