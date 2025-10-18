0413e8be3d1ca24198aeb21ad5f0bf0954b8eb94
myciah <rus21019@byui.edu>
2025-10-17 22:59:54 -0600

    Develop05 commit11

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -10,3 +10,25 @@ using System;
 //}
 
 
+//Testing CheckListGoal.cs
+class program
+{
+    static void main
+    {
+        CheckListGoal g = new CheckListGoal("Physical", "20 min", 10, 3, 50);
+        Console.WriteLine("Created!");
+        Console.WriteLine(g.GetDetail());
+        Console.WriteLine("Record 1st: ");
+        g.RecordEvent();
+        Console.WriteLine(g.GetUpdate());
+        Console.WriteLine("Recording 2nd: ");
+        g.RecordEvent();
+        console.WriteLine(g.GetUpdate());
+        Console.WriteLine(Recording 3rd: );
+        g.RecordEvent();
+        Console.WriteLine(g.GetUpdate());
+        Console.WriteLine("Record 4th (b): ");
+        g.RecordEvent();
+        Console.WriteLine(g.GetUpdate());
+    }
+}
