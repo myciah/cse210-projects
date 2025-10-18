c679b00f821a7b4112eef8c2f9b4fc786229fdfb
myciah <rus21019@byui.edu>
2025-10-17 19:19:59 -0600

    Develop05 commit3

---
+++ prove/Develop05/SimpleGoal.cs
@@ -0,0 +1,36 @@
+using System.Drawing;
+
+class SimpleGoal : Goal
+{
+    private bool _c;
+    public SimpleGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+        _c = false;
+    }
+    public override int RecordEvent()
+    {
+        _c = true;
+        Console.WriteLine($"Goal Complete!! {points} points");
+        return points;
+    }
+    public override bool IsComplete()
+    {
+        return c;
+    }
+    public override string GetUpdate()
+    {
+        if (_c = true)
+        {
+            return ($"[X] {name}")
+        }
+        else
+        {
+            return ($"[] {name}")
+        }
+    }
+    public override string GetDetail()
+    {
+        ($"{name} - {description} {points} pts");
+    }
+}
