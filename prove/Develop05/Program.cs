771afc31c2957955d0bb58ef5da3e1527e62ccef
myciah <rus21019@byui.edu>
2025-10-18 03:24:28 -0600

    Develop05 commit18

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,41 +1,125 @@
 using System;
-
-//class Program
-//{
-//  static void Main(string[] args)
-//{
-
-//}
-
-//}
-
-
-class program
+class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
         GoalManager man = new GoalManager();
-        SimpleGoal g1 = new SimpleGoal("Reading", "Read Chapter", 10);
-        EternalGoal g2 = new EternalGoal("Eat Healthy", "Drink Water", 5);
-        CheckListGoal g3 = new CheckListGoal("Exercise", "Leg Day", 15, 3, 40);
-        man.AddGoal(g1);
-        man.AddGoal(g2);
-        man.AddGoal(g3);
-        Console.WriteLine("Goals");
+        bool running = true;
+        while (running)
+        {
+            Console.WriteLine("Eternal Quest Menu");
+            Console.WriteLine("1. Create New Goal");
+            Console.WriteLine("2. Record Event");
+            Console.WriteLine("3. Show Goals");
+            Console.WriteLine("4. Show Score");
+            Console.WriteLine("5. Save Goals");
+            Console.WriteLine("6. Load Goals");
+            Console.WriteLine("7. Exit");
+            Console.Write("Choose an option: ");
+            string option = Console.ReadLine();
+
+            if (option == "1")
+            {
+                CreGo(man);
+            }
+            else if (option == "2")
+            {
+                RecordEvent(man);
+            }
+            else if (option == "3")
+            {
+                man.DisplayGo();
+            }
+            else if (option == "4")
+            {
+                man.DisplaySc();
+            }
+            else if (option == "5")
+            {
+                SaveGo(man);
+            }
+            else if (option == "6")
+            {
+                LoadGo(man);
+            }
+            else if (option == "7")
+            {
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("Please choose a valid option.");
+            }
+        }
+        Console.WriteLine("See you next time!!");
+    }
+    static void CreGo(GoalManager man)
+    {
+        Console.Write("What is the type of goal? (simple / eternal / checklist): ");
+        string ty = Console.ReadLine();
+        Console.Write("Name: ");
+        string na = Console.ReadLine();
+        Console.Write("Description: ");
+        string des = Console.ReadLine();
+        string points = HowMany("How many points does this goal get? ");
+
+        if (ty == "simple")
+        {
+            SimpleGoal SG =  SimpleGoal(na, des, points);
+            man.AddGoal(SG);
+            Console.WriteLine("A simple goal was created.");
+        }
+        else if (ty == "eternal")
+        {
+            EternalGoal EG = new EternalGoal(na, des, points);
+            man.AddGoal(EG);
+            Console.WriteLine("An eternal goal was created.");
+        }
+        else if(ty == "checklist")
+        {
+            int Tar = HowMany("How many times do you need to complete? ");
+            int Bon = HowMany("Bonus points??? ");
+            CheckListGoal CG = new CheckListGoal(na, des, point, Tar, Bon);
+            man.AddGoal(CG);
+            Console.WriteLine("Checklist goal has been created.");
+        }
+        else
+        {
+            Console.WriteLine("Unkown");
+        }
+    }
+    static void RecordEvent(GoalManager man)
+    {
         man.DisplayGo();
-        man.DisplaySc();
-        Console.WriteLine("Recording events");
-        man.RecordEvent(1);
-        man.RecordEvent(2);
-        man.RecordEvent(3);
-        man.RecordEvent(3);
-        man.RecordEvent(3);
-        Console.WriteLine("After Recording");
-        man.DisplayGo();
-        man.DisplaySc();
-        Console.WriteLine("Saving to file");
-        man.SaveGo("go.txt");
-        Console.WriteLine("Loading goals");
-        man.LoadGo("go.txt");
+        int ind = HowMany("Enter the goal number to record: ");
+        man.RecordEvent(ind);
+    }
+    static void SaveGo(GoalManager man)
+    {
+        Console.Write("Enter the filename to save: ");
+        string filename = Console.ReadLine();
+        man.SaveGo(filename);
+    }
+    static void LoadGo(GoalManager man)
+    {
+        Console.Write("Enter filename to load: ");
+        string filename = Console.ReadLine();
+        man.LoadGo(filename);
+    }
+    static string HowMany(string quest)
+    {
+        while (true)
+        {
+            Console.Write(Quest);
+            string inp = Console.ReadLine();
+            int value;
+            if (int.TryParse(inp, out value))
+            {
+                return value;
+            }
+            Console.WriteLine("Please enter a number that is valid.");
+        }
     }
 }
+
+
