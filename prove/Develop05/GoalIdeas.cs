1a655324ae80c76d28945bf8e58a53950dd1ff3f
myciah <rus21019@byui.edu>
2025-10-18 14:33:03 -0600

    Develop05 commit20

---
+++ prove/Develop05/GoalIdeas.cs
@@ -0,0 +1,24 @@
+class GoalIdeas
+{
+    private List<string> _i = new List<string>()
+    {
+        "Exercise for 30 Minutes",
+        "Drink 8 cups of water daily",
+        "Do the dishes/clean kitchen",
+        "Write in journal",
+        "Read General Conference Talk (x5)",
+        "Read scriptures Daily",
+        "Reach out to a friend"
+        "Complement others (x15)",
+        "Have two meaningful prayers everyday",
+        "Help at a service project each week";
+    }
+    public void DisplayId()
+    {
+        Console.WriteLine("Hera are some goal ideads you could use: ");
+        while (int i = 0; i < _i.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}. {_i[i]}");
+        }
+    }
+}
