23381aac73af2f5c5fc31d5431e2f2c35796cca5
myciah <rus21019@byui.edu>
2025-10-17 20:54:04 -0600

    Develop05 commit6

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -10,18 +10,3 @@ using System;
 //}
 
 
-//Folowing code used to test if SimpleGoal.cs works
-class program
-{
-    static void Main()
-    {
-        SimpleGoal SGoal = new SimpleGoal("Read Scriptures", "Read chapter today", 10);
-        Console.WriteLine("Goal Detail: ");
-        Console.WriteLine(SGoal.GetDetail());
-        Console.WriteLine($"Goal Complete? {SGoal.IsComplete()}");
-        int PE = SGoal.RecordEvent();
-        Console.WriteLine($"Points: {PE}");
-        Console.WriteLine($"Is goal complete? {SGoal.IsComplete()}");
-        Console.WriteLine($"Goal Update: {SGoal.GetUpdate()}");
-    }
-}
--- prove/Develop05/SimpleGoal.cs
+++ prove/Develop05/SimpleGoal.cs
@@ -32,3 +32,22 @@ class SimpleGoal : Goal
         return ($"{_n} - {_d} {_p} pts");
     }
 }
+
+
+
+
+//The Following code was used to test if SimpleGoal.cs in program.cs functioned as it was supposed to.
+// class program
+// {
+//     static void Main()
+//     {
+//         SimpleGoal SGoal = new SimpleGoal("Read Scriptures", "Read chapter today", 10);
+//         Console.WriteLine("Goal Detail: ");
+//         Console.WriteLine(SGoal.GetDetail());
+//         Console.WriteLine($"Goal Complete? {SGoal.IsComplete()}");
+//         int PE = SGoal.RecordEvent();
+//         Console.WriteLine($"Points: {PE}");
+//         Console.WriteLine($"Is goal complete? {SGoal.IsComplete()}");
+//         Console.WriteLine($"Goal Update: {SGoal.GetUpdate()}");
+//     }
+// }
