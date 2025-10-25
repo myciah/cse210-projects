44d21f8d7cca4cf355b68b8969ae71e7112b65ad
myciah <rus21019@byui.edu>
2025-10-25 15:50:12 -0600

    Foundation2 commit4

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -14,11 +14,11 @@ class Program
      static void Main(string[] args)
     {
         Product P = new Product("Roller Blades", "XXX123", 122.99, 1);
-        Console.WriteLine("Product Name: {P.Gname()}");
-        Console.WriteLine("Product ID: {P.GProductId()}");
-        Console.WriteLine("Product Price: {P.GPrice()}");
-        Console.WriteLine("Product Quantity: {P.GQuantity()}");
-        Console.WriteLine("Total: {P.GTotCost()}");
+        Console.WriteLine($"Product Name: {P.GName()}");
+        Console.WriteLine($"Product ID: {P.GProductId()}");
+        Console.WriteLine($"Product Price: {P.GPrice()}");
+        Console.WriteLine($"Product Quantity: {P.GQuantity()}");
+        Console.WriteLine($"Total: {P.GTotCost()}");
 
     }
 
