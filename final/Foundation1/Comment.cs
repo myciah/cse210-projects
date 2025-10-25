4a994429ea973e5f68f4a1f5a3e5e291c7232efa
myciah <rus21019@byui.edu>
2025-10-24 20:20:28 -0600

    Foundation1 commit1

---
+++ final/Foundation1/Comment.cs
@@ -0,0 +1,18 @@
+public class Comment
+{
+    private string comName;
+    private string comText;
+    public comment(string Name, string Text)
+    {
+        comName = Name;
+        comText = Text;
+    }
+    public string GComName()
+    {
+        return comName;
+    }
+    public string GComText()
+    {
+        return comText;
+    }
+}
