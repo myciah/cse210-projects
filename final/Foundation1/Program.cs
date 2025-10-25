e1b4e8c06f74e14d1d5372ecc47227a7a0480dd0
myciah <rus21019@byui.edu>
2025-10-24 21:22:43 -0600

    Foundation1 commit5

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -8,3 +8,13 @@ using System;
 //     }
 // }
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Video MV = new Video("Let's do this", "Charlie");
+        Console.WriteLine($"Title: {MV.GTitle()}");
+        Console.WriteLine($"Author: {MV.GAuthor()}");
+        Console.WriteLine($"#Comments: {MV.GComCount()}");
+    }
+}
