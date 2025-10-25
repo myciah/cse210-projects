3710ccba24d8d5a54bab78a08f756f1ebf4b6c0c
myciah <rus21019@byui.edu>
2025-10-25 16:15:40 -0600

    Foundation2 commit6

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -1,1 +1,22 @@
-public 
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _stateProv;
+    private string country;
+    public Address(string street, string city, string StateProv, string country)
+    {
+        _street = street;
+        _city = city;
+        _stateProv = StateProv;
+        _country = country;
+    }
+    public bool USA()
+    {
+        return _country;
+    }
+    public string GFullAddress()
+    {
+        return ($"{_street} - {_city} - {_stateProv} - {_country}");
+    }
+}
