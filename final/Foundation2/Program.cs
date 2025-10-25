9194e32ba06ee34b70330ecf85498ee0a795f672
myciah <rus21019@byui.edu>
2025-10-25 16:28:49 -0600

    Foundation2 commit7

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -8,3 +8,30 @@ using System;
 //     }
 // }
 
+class Program
+{
+     static void Main(string[] args)
+    {
+        Address A1 = new Address("123 XYZ St", "Arlington", "VA", "USA");
+        Console.WriteLine($"Address: {A1.GFullAddress()}");
+        if (A1.USA() == "USA")
+        {
+            Console.WriteLine("This address is in the USA.");
+        }
+        else
+        {
+            Console.WriteLine("Not USA Address");
+        }
+        Address A2 = new Address("456 ZYX st", "Tokyo", "Shinjuku", "Japan");
+        Console.WriteLine($"Address: {A2.GFullAddress}");
+        if (A2.USA() == "USA")
+        {
+            Console.WriteLine("This address is in the USA.");
+        }
+        else
+        {
+            Console.WriteLine("Not USA Address");
+        }
+    }
+
+}
