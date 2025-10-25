135dddad2f53f469c70f2d7d3ec1afa67895336b
myciah <rus21019@byui.edu>
2025-10-25 17:00:48 -0600

    Foundation2 commit9

--- final/Foundation2/Address.cs
+++ final/Foundation2/Address.cs
@@ -20,3 +20,32 @@ public class Address
         return ($"{_street} - {_city} - {_stateProv} - {_country}");
     }
 }
+
+//The following code was used to test Address.cs in Program.cs
+// class Program
+// {
+//      static void Main(string[] args)
+//     {
+//         Address A1 = new Address("123 XYZ St", "Arlington", "VA", "USA");
+//         Console.WriteLine($"Address: {A1.GFullAddress()}");
+//         if (A1.USA())
+//         {
+//             Console.WriteLine("This address is in the USA.");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Not USA Address");
+//         }
+//         Address A2 = new Address("456 ZYX st", "Tokyo", "Shinjuku", "Japan");
+//         Console.WriteLine($"Address: {A2.GFullAddress()}");
+//         if (A2.USA())
+//         {
+//             Console.WriteLine("This address is in the USA.");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Not USA Address");
+//         }
+//     }
+
+// }
