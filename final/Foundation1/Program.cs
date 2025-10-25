79e9332cfedb05be57afe189bc98a55122139fad
myciah <rus21019@byui.edu>
2025-10-24 20:33:12 -0600

    Foundation1 commit2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,20 @@
 using System;
 
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Foundation1 World!");
+//     }
+// }
+
+//Next lines of code are to test the comment class:
+using System;
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation1 World!");
+    static void Main(string[] )args){
+        Comment TC = new Comment("John", "This is a test!");
+        Console.WriteLine($"Name: {TC.GComName}");
+        Console.WriteLine($"Text: {TC.GComText}");
     }
 }
