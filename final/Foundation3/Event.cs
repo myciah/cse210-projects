11ea9c76c538515b8f15075e694386fdea904938
myciah <rus21019@byui.edu>
2025-10-27 16:59:23 -0600

    Foundation3 commit1

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,17 @@
+public abstract class Event
+{
+    private string _title;
+    private string desc;
+    private string _time;
+    public Event(string title, string, desc, string date, string time)
+    {
+        _title = title;
+        _desc = desc;
+        _date = date;
+        _time = time;
+    }
+    public string GStandard()
+    {
+        return ($"")
+    }
+}
