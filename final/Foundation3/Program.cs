0cf1141816a30312405c961858ea897d88107a12
myciah <rus21019@byui.edu>
2025-10-27 17:54:15 -0600

    Foundation3 commit6

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,21 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Foundation3 World!");
-    }
-}
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello Foundation3 World!");
+//     }
+// }
+
+//The following code is to test the Lecutre class
+ class Program
+ {
+     static void Main(string[] args)
+     {
+        Address TA = new Address("123 XYZ Rd", "YYYYYY", "VA", "USA");
+        Lecture TL = new Lecture("Heidi Russell", 45);
+
+        Console.WriteLine(TL.GFullLecture());
+     }
+ }
