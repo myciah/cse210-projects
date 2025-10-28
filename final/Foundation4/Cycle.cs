e94ead67b8652e072a374b77863dae417e1cb9e6
myciah <rus21019@byui.edu>
2025-10-28 00:48:27 -0600

    Foundation4 commit6

--- final/Foundation4/Cycle.cs
+++ final/Foundation4/Cycle.cs
@@ -1,22 +1,22 @@
 public class Cycle : Activity
 {
-    private double _Speed;
+    private double _speed;
     public Cycle(string date, int minutes, double speed)
-        : base(date.minutes)
+        : base(date, minutes)
     {
         _speed = speed;
         _activityType = "Cycling";
     }
     public override double GSpeed()
     {
-        return _Speed;
+        return _speed;
     }
     public override double GPace()
     {
-        return 60 / _Speed;
+        return 60 / _speed;
     }
     public override double GDist()
     {
-        return (_Speed * GMinutes()) \ 60;
+        return (_speed * GMinutes()) / 60;
     }
 }
