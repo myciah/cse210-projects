af081990556e91fa6e536e64d06de296a43c5f7a
myciah <rus21019@byui.edu>
2025-10-27 20:37:19 -0600

    Foundation3 commit15

--- final/Foundation3/Outdoor.cs
+++ final/Foundation3/Outdoor.cs
@@ -7,8 +7,21 @@ public class Outdoor : Event
         _weather = weather;
         _eventType = "Outdoor";
     }
-        public string GFullOutdoor()
+    public string GFullOutdoor()
     {
         return ($"{GStandard()} - Type: {_eventType} - Weather: {_weather}");
     }
 }
+
+//The following code was used to test Outdoor.cs in program.cs
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Address TA = new Address("123 XYZ Rd", "YYYYYY", "VA", "USA");
+//         Outdoor TO = new Outdoor("Volleyball", "Bracket for the trophy", "10-27-2025", "6 pm", TA, "Rainy");
+//         Console.WriteLine(TO.GStandard());
+//         Console.WriteLine(TO.GFullOutdoor());
+//         Console.WriteLine(TO.GShort());
+//     }
+// }
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,23 +1,33 @@
 using System;
-
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine("Hello Foundation3 World!");
-//     }
-// }
-
-//The following code is to test the Lecutre class
 class Program
-{
-    static void Main(string[] args)
-    {
-        Address TA = new Address("123 XYZ Rd", "YYYYYY", "VA", "USA");
-        Outdoor TO = new Outdoor("Volleyball", "Bracket for the trophy", "10-27-2025", "6 pm", TA, "Rainy");
-        Console.WriteLine(TO.GStandard());
-        Console.WriteLine(TO.GFullOutdoor());
-        Console.WriteLine(TO.GShort());
-    }
+ {
+     static void Main(string[] args)
+     {
+        Address A1 = new Address("123 Elysian blvd", "Krat", "CVDL", "France");
+        Address A2 = new Address("456 Malum Ave", "Slums", "Corsica", "France");
+        Address A3 = new Address("789 Tower Isle", "Alchemist Island", "Occitanie", "France");
+
+        Event lecture = new Lecture("Lorenzini Venigni", 10, "A New Era", "Learning about advanced technology", "10-27-2025", "7:00 PM", A1);
+        Event reception = new Reception("Reunion", "Carlo and Lea", "11-28-2025", "4:00 PM", A2, "Pulcinella@VenigniWorks.com");
+        Event outdoor = new Outdoor("Sparring Session", "Test the Monad's Rose Sword", "12-29-2025", "9:00 AM", A3, "Snowey");
+
+        List<Event> E = new List<Event>
+        {
+            lecture,
+            reception,
+            outdoor
+        };
+        
+        foreach (Event i in E)
+        {
+            Console.WriteLine("Standard Details: ");
+            Console.WriteLine(i.GStandard());
+            Console.WriteLine();
+            Console.WriteLine("Full Details: ");
+            Console.WriteLine("Blank");
+            Console.WriteLine();
+            Console.WriteLine("Short Description: ");
+            Console.WriteLine(i.GShort());
+        }
+     }
 }
- 
