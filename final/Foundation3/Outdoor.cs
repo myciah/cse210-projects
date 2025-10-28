36e7244c9c38ff3e6acc41fe98e760c5b69951b3
myciah <rus21019@byui.edu>
2025-10-27 19:56:32 -0600

    Foundation3 commit14

--- final/Foundation3/Outdoor.cs
+++ final/Foundation3/Outdoor.cs
@@ -1,13 +1,14 @@
 public class Outdoor : Event
 {
     private string _weather;
-    public Outdoor(string title, string desc, string date, string time, Address addr, string weather);
-        base : (title, desc, date, time, addr)
-        {
-            _weather = weather;
-        }
+    public Outdoor(string title, string desc, string date, string time, Address addr, string weather)
+        : base(title, desc, date, time, addr)
+    {
+        _weather = weather;
+        _eventType = "Outdoor";
+    }
         public string GFullOutdoor()
-        {
-    ($"{GStandard()} - Type: {_eventType} - Weather: {_weather}");
-        }
+    {
+        return ($"{GStandard()} - Type: {_eventType} - Weather: {_weather}");
+    }
 }
