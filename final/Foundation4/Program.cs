53b0ea8caa6acb3cfecd836e6542498e8408685d
myciah <rus21019@byui.edu>
2025-10-28 01:18:18 -0600

    Foundation4 commit8

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,24 +1,18 @@
 using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine("Hello Foundation4 World!");
-//     }
-// }
-
-//Following code to test run.cs
-class Program
-{
-    static void Main(string[] args)
-    {
-        Cycle TS = new Cycle("27 Oct 2025", 60, 2);
+ class Program
+ {
+     static void Main(string[] args)
+     {
         List<Activity> act = new List<Activity>();
-        act.Add(TS);
-        foreach (Activity a in act)
+        act.Add(new Run("01 Jan 2023", 45, 3.5));
+        act.Add(new Cycle("02 Feb 2024", 60, 14));
+        act.Add(new("03 Mar 2025", 75, 5));
+
+        foreach (Activity ac in act)
         {
-            Console.WriteLine(a.GSummary());
+            Console.WriteLine(ac.GSummary());
         }
-    }
-}
+     }
+ }
+
--- final/Foundation4/Swim.cs
+++ final/Foundation4/Swim.cs
@@ -12,3 +12,18 @@ public class Swim : Activity
         return _laps * 50 / 1000 * .62;
     }
 }
+
+//The following code was used to test Swim.cs in program.cs
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Cycle TS = new Cycle("27 Oct 2025", 60, 2);
+//         List<Activity> act = new List<Activity>();
+//         act.Add(TS);
+//         foreach (Activity a in act)
+//         {
+//             Console.WriteLine(a.GSummary());
+//         }
+//     }
+// }
