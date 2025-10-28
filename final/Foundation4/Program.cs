5feab9958112c96e475581bfd0f18f14dcd2b0cc
myciah <rus21019@byui.edu>
2025-10-28 01:21:23 -0600

    Foundation4 commit9

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -7,7 +7,7 @@ using System;
         List<Activity> act = new List<Activity>();
         act.Add(new Run("01 Jan 2023", 45, 3.5));
         act.Add(new Cycle("02 Feb 2024", 60, 14));
-        act.Add(new("03 Mar 2025", 75, 5));
+        act.Add(new Swim("03 Mar 2025", 75, 5));
 
         foreach (Activity ac in act)
         {
