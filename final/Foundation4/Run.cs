5821b793485adbb54491ba9fe3a09d368fe516e4
myciah <rus21019@byui.edu>
2025-10-27 23:53:32 -0600

    Foundation4 commit2

---
+++ final/Foundation4/Run.cs
@@ -0,0 +1,15 @@
+public class Run : Activity
+{
+    private double _dist;
+
+    public Run(string date, int minutes, double dist)
+        : base(date, minutes)
+    {
+        _dist = dist;
+        _activityType = "Running";
+    }
+    public override double GDist()
+    {
+        return dist;
+    }
+}
